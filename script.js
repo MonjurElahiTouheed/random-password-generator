60dbcd102dee585378c1f3288ee9f7af84fdbf45
MonjurElahiTouheed <monjurelahitouheed@gmail.com>
2025-03-18 22:00:27 +0600

    copy button made

--- script.js
+++ script.js
@@ -5,16 +5,68 @@ const minusBtn = document.querySelector('.minus__btn');
 const password = document.querySelector('.password');
 const upperCase = document.getElementById('upper__case');
 const lowerCase = document.getElementById('lower__case');
-
+const symbol = document.getElementById('symbol');
+const number = document.getElementById('number');
+const copyBtn = document.getElementById('copy__btn');
 plength.value = range.value;
 
-const charSet = 'ABCDEFGHIJKLMNOPQRSTUVWXYXZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+{}|[]\/.,><~`'
-let newPassWord = '';
+let charSet = 'ABCDEFGHIJKLMNOPQRSTUVWXYXZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+{}|[]\/.,><~`';
 
+function checkMark() {
+    if(upperCase.checked && lowerCase.checked && number.checked && symbol.checked){
+        charSet = 'ABCDEFGHIJKLMNOPQRSTUVWXYXZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+{}|[]\/.,><~`';
+    }
+    else if(upperCase.checked && lowerCase.checked && number.checked){
+        charSet ='ABCDEFGHIJKLMNOPQRSTUVWXYXZabcdefghijklmnopqrstuvwxyz0123456789';
+    }
+    else if(upperCase.checked && lowerCase.checked && symbol.checked){
+        charSet = 'ABCDEFGHIJKLMNOPQRSTUVWXYXZabcdefghijklmnopqrstuvwxyz!@#$%^&*()_+{}|[]\/.,><~`';
+    }
+    else if(upperCase.checked && number.checked && symbol.checked){
+        charSet = 'ABCDEFGHIJKLMNOPQRSTUVWXYXZ!@#$%^&*()_+{}|[]\/.,><~`';
+    }
+    else if(lowerCase.checked && number.checked && symbol.checked){
+        charSet = 'abcdefghijklmnopqrstuvwxyz!@#$%^&*()_+{}|[]\/.,><~`';
+    }
+    else if(upperCase.checked && lowerCase.checked){
+        charSet ='ABCDEFGHIJKLMNOPQRSTUVWXYXZabcdefghijklmnopqrstuvwxyz';
+    }
+    else if(upperCase.checked && number.checked){
+        charSet ='ABCDEFGHIJKLMNOPQRSTUVWXYXZ0123456789';
+    }
+    else if(upperCase.checked && symbol.checked){
+        charSet ='ABCDEFGHIJKLMNOPQRSTUVWXYXZ!@#$%^&*()_+{}|[]\/.,><~`';
+    }
+    else if(lowerCase.checked && number.checked) {
+        charSet ='abcdefghijklmnopqrstuvwxyz0123456789';
+    }
+    else if(lowerCase.checked && symbol.checked) {
+        charSet ='abcdefghijklmnopqrstuvwxyz!@#$%^&*()_+{}|[]\/.,><~`';
+    }
+    else if(number.checked && symbol.checked) {
+        charSet = '0123456789!@#$%^&*()_+{}|[]\/.,><~`';
+    }
+    else if(upperCase.checked){
+        charSet = 'ABCDEFGHIJKLMNOPQRSTUVWXYXZ';
+    }
+    else if(lowerCase.checked){
+        charSet = 'abcdefghijklmnopqrstuvwxyz';
+    }
+    else if(number.checked){
+        charSet = '0123456789';
+    }
+    else if(symbol.checked){
+        charSet = '!@#$%^&*()_+{}|[]\/.,><~`';
+    }
+   
+    // generatePass();
+}
+checkMark();
 
 
 console.log(charSet.length);
 
+let newPassWord = '';
 
 
 
@@ -27,7 +79,7 @@ function generatePass(){
         newPassWord += charSet[index];
     }
     
-    password.innerText = newPassWord;
+    password.value = newPassWord;
     newPassWord='';
 }
 
@@ -53,4 +105,10 @@ const minus = () => {
 function rangeNumber() {
     range.value = plength.value;
     generatePass();
+}
+
+function copy() {
+    password.select();
+    navigator.clipboard.writeText(password.value);
+    
 }
